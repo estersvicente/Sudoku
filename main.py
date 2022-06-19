3e97e2fc933b7e267d5d9b282e7e8de9b83f5a13
estersvicente <estersvicente@gmail.com>
2022-06-19 19:05:43 -0400

    user window

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from sudoku import Sudoku
+from sudoku_generator import SudokuGenerator
 
-s = Sudoku()
+s = SudokuGenerator()
 play = input('Hello! Welcome to Sudoku! \nDo Want a Sudoku Board? (type "y" for Yes and "n" for No) \n')
 if play.lower() == 'y':
-    s.generate_board()
-    s.print_grid()
-else:
-    print('Ok, goodbye!')
-
-solution = input('Would you like the solution? (type "y" for Yes and "n" for No)\n')
-if solution.lower() == 'y':
-    print(s)
+    s.get_sudoku()
 else:
     print('Ok, goodbye!')
