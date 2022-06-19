3d08f41e061de35095faea45b8da0f61ff1625f5
estersvicente <estersvicente@gmail.com>
2022-06-19 14:17:09 -0400

    User window

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-$ git config --global user.email estersvicente@gmail.com
+from sudoku import Sudoku
+
+s = Sudoku()
+play = input('Hello! Welcome to Sudoku! \nDo Want a Sudoku Board? (type "y" for Yes and "n" for No) \n')
+if play.lower() == 'y':
+    s.generate_board()
+    s.print_grid()
+else:
+    print('Ok, goodbye!')
+
+solution = input('Would you like the solution? (type Y for Yes and N for No)\n')
+if solution.upper() == 'Y':
+    print(s)
+    
