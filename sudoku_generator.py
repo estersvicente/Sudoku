ef591f134aa5c66253a9b3b2d4f85bf741931d20
estersvicente <estersvicente@gmail.com>
2022-06-19 20:09:01 -0400

    final sudoku generator

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,27 +1,31 @@
 from sudoku import Sudoku
 from sudoku_solver import SudokuSolver
-import numpy as np
 
 
 class SudokuGenerator:
     def __init__(self):
         self.g = Sudoku()
         self.s = SudokuSolver()
+        self.solution = ''
 
     def get_sudoku(self):
         sg = SudokuGenerator()
-        mat1 = np.array(self.g.generate_board())
+        self.g.generate_board()
         board = self.g.get_partial_sudoku_matrix()
-        mat2 = np.array(self.s.solve_sudoku(board))
-        if np.array_equal(mat1, mat2):
+        self.s.solve_sudoku(board)
+        if str(self.s) == str(self.g):
             self.g.print_grid()
+            self.solution += self.s.solution
+            print(self.solution)
         else:
             sg.get_sudoku()
 
+    def print_solution(self):
+        print(self.s)
+        print(self.g)
 
-    def get_solution(self):
-        solution = input('Would you like the solution? (type "y" for Yes and "n" for No)\n')
-        if solution.lower() == 'y':
-            print(self.s)
-        else:
-            print('Ok, goodbye!')
+    def __str__(self):
+        return self.solution
+
+
+
