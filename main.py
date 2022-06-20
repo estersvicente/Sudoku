5660d43852678f634aeccfe5bfb778c1a129abbe
estersvicente <estersvicente@gmail.com>
2022-06-19 21:15:45 -0400

    User Prompt Window

--- main.py
+++ main.py
@@ -6,12 +6,3 @@ if play.lower() == 'y':
     s.get_sudoku()
 else:
     print('Ok, goodbye!')
-
-solution = input('Would you like the solution? (type "y" for Yes and "n" for No)\n')
-if solution.lower() == 'y':
-    s.print_solution()
-else:
-    print('Ok, goodbye!')
-
-print('\n')
-print(s)
