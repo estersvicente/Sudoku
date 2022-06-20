78a22fdbb59f2b5a70689b6c3df371d47ef285d8
estersvicente <estersvicente@gmail.com>
2022-06-19 21:17:20 -0400

    Final Sudoku generator (single solution)

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -6,26 +6,21 @@ class SudokuGenerator:
     def __init__(self):
         self.g = Sudoku()
         self.s = SudokuSolver()
-        self.solution = ''
 
     def get_sudoku(self):
+        result = ''
         sg = SudokuGenerator()
         self.g.generate_board()
         board = self.g.get_partial_sudoku_matrix()
         self.s.solve_sudoku(board)
         if str(self.s) == str(self.g):
             self.g.print_grid()
-            self.solution += self.s.solution
-            print(self.solution)
+
+            solution = input('Would you like the solution? (type "y" for Yes and "n" for No)\n')
+            if solution.lower() == 'y':
+                print(self.s)
+            else:
+                print('Ok, goodbye!')
         else:
             sg.get_sudoku()
-
-    def print_solution(self):
-        print(self.s)
-        print(self.g)
-
-    def __str__(self):
-        return self.solution
-
-
 
